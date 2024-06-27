8c7bb18e717f527e18a96799bad7dc560bcef044
bruhoffman <bruhoffman@gmail.com>
2024-06-27 11:50:30 -0300

    Feito os exercícios de fixação

---
+++ src/types.ts
@@ -0,0 +1,15 @@
+export type TCustomer = {
+    id: string,
+    name: string,
+    email: string,
+    password: string,
+    createAt: string
+}
+
+export type TProduct = {
+    id: string,
+    name: string,
+    price: number,
+    description: string,
+    imageUrl: string
+}
